a1ce973abb9ec6c0c71c247f5eb20452972ee8ee
brinenick511-venus <brinenick511@gmail.com>
2024-05-05 16:25:23 +0800

    modified:   ptuning/eval_ptuning.sh

--- ptuning/eval_ptuning.sh
+++ ptuning/eval_ptuning.sh
@@ -1,5 +1,5 @@
 PRE_SEQ_LEN=128
-CHECKPOINT=adgen-chatglm-6b-pt-128-2e-2
+CHECKPOINT=pt-4b-128-2e-2
 STEP=3000
 
 CUDA_VISIBLE_DEVICES=0 python3 main.py \
@@ -9,8 +9,8 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py \
     --overwrite_cache \
     --prompt_column content \
     --response_column summary \
-    --model_name_or_path /data/yanghq/models/YHQ/chatglm-6b \
-    --ptuning_checkpoint ./output/$CHECKPOINT/checkpoint-$STEP \
+    --model_name_or_path /data/yanghq/models/THUDM/chatglm-6b \
+    --ptuning_checkpoint /data/yanghq/outputs/$CHECKPOINT/checkpoint-$STEP \
     --output_dir ./output/$CHECKPOINT \
     --overwrite_output_dir \
     --max_source_length 64 \
@@ -18,4 +18,5 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py \
     --per_device_eval_batch_size 1 \
     --predict_with_generate \
     --pre_seq_len $PRE_SEQ_LEN \
-    --quantization_bit 4
+    --preprocessing_num_workers 16 \
+    --quantization_bit 4 
